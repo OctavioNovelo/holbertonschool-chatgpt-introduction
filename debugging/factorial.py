aa475d0ff37ea6d4c9ce235ce6cf08a64b48bd3e
OctavioNovelo <octavionovelo22@gmail.com>
2024-05-07 17:36:27 +0000

    Task0

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,17 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n = n - 1
-    return result
+        n -= 1
+         return result
 
-if len(sys.argv) != 2:
-    print("Uso: python script.py <numero>")
-    sys.exit(1)
-
-try:
-    num = int(sys.argv[1])
-except ValueError:
-    sys.exit(1)
-
-f = factorial(num)
+f = factorial(int(sys.argv[1]))
 print(f)
